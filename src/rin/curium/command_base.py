9f74aa2bf9964f14c74ece6373499f95d860979c
Rin <susautw@gmail.com>
2022-08-26 13:35:45 +0800

    Add __cmd_autoname__ to select how to generate the command name.

--- src/rin/curium/command_base.py
+++ src/rin/curium/command_base.py
@@ -11,11 +11,18 @@ T = TypeVar("T")
 
 class CommandBase(cfg.BaseConfig, Generic[T], ABC):
 
-    __cmd_name__ = "command_base"
+    __cmd_name__ = "__cmd_command_base__"
+    __cmd_autoname__ = "module"
 
     def __init_subclass__(cls, **kwargs):
         if "__cmd_name__" not in vars(cls):
-            setattr(cls, "__cmd_name__", cls.__name__)
+            if cls.__cmd_autoname__ == "class":
+                cls.__cmd_name__ = cls.__name__
+            elif cls.__cmd_autoname__ == "module":
+                cls.__cmd_name__ = f'{cls.__module__}.{cls.__name__}'
+            else:
+                raise ValueError("__cmd_autoname__ should be 'class' or 'module'."
+                                 " If you intend to specify a command name, you should use __cmd_name__")
 
         __cmd_name__ = getattr(cls, "__cmd_name__")
         __cmd_name_option__ = cfg.Lazy(lambda c: __cmd_name__, name="__cmd_name__")
